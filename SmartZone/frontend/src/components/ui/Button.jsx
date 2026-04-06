f438f57f5983a22d5e8b459a5fea9c8f7e3f68d0
Pamidu <routerunlockking@gmail.com>
2026-04-06 13:06:39 +0530

    Delete SmartZone directory

--- SmartZone/frontend/src/components/ui/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ children, onClick, disabled, className = '', type = 'button' }) {
-  return (
-    <button type={type} onClick={onClick} disabled={disabled} className={`px-4 py-3 rounded-xl font-medium transition-all ${disabled ? 'opacity-50 cursor-not-allowed' : 'hover:opacity-90'} ${className}`}>
-      {children}
-    </button>
-  );
-}
