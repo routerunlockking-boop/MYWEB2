2971c922426b3c92622b83e65bee98e4e66edcad
Pamidu <routerunlockking@gmail.com>
2026-04-06 13:05:42 +0530

    Update db.js

--- SmartZone/backend/config/db.js
+++ SmartZone/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.mongodb+srv://Admin:Admin%4012345@cluster0.czllghf.mongodb.net/myDatabase?retryWrites=true&w=majority);
     console.log('✅ MongoDB සම්බන්ධ විය');
   } catch (err) {
     console.error('❌ MongoDB සම්බන්ධ වීම අසාර්ථකයි:', err.message);
